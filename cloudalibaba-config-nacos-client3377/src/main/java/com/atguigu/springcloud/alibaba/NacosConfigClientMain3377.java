48032a4fad22c254e594dacffa8178980bea6eb2
HeChi <2249133537@qq.com>
2022-11-11 15:01:26 +0800

    Nacos服务配置中心 明白namespace > group >dataID概念， 能精确找到nacos上配置的文件

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: HeChi
+ * @Date: 2022/11/11
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
+    }
+}
